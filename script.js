20b4ab1fc4ae565c790ed3c38a65270bac51239a
Bradyn <bradyn.lv@gmail.com>
2024-08-01 10:58:01 -0700

    Add input features

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function saveLead() {
-    console.log("Button clicked!")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    const inputValue = inputEl.value
+    myLeads.push(inputValue)
+    console.log(myLeads)
+})
+
+for (let i = 0; i < myLeads.length; i++) {
+    const li = document.createElement("li")
+    li.textContent = myLeads[i]
+    ulEl.append(li)
 }
