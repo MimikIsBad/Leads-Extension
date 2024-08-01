d556ef9a02f3207c16260fff962506aec1de2953
Bradyn <bradyn.lv@gmail.com>
2024-08-01 11:23:44 -0700

    Add styling

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    const inputValue = inputEl.value
-    myLeads.push(inputValue)
-    console.log(myLeads)
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li)
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+      //  listItems += "<li>" + "<a href=" + myLeads[i] + "target='_blank'>" + myLeads[i] + "</a>" + "</li>"
+        listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>     
+                ${myLeads[i]}
+            </a>
+        </li>
+        `
+    }
+    ulEl.innerHTML = listItems
 }
